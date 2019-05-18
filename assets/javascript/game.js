8b8001077adf325945e219af6a32863958c42df4
Giselle Malenchek <Giselle@Giselles-MBP.lan>
2019-05-18 16:25:37 -0400

    Solved logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,55 @@
-var score = 0;
-//Print score
-$("#score").append(score);
+$(document).ready(function () {
+    //Initialize variables
+    var random;
+    var score = 0;
+    var wins = 0;
+    var losses = 0;
+    //Array of crystals
+    var crystals = ["#emerald", "#turquoise", "#diamond", "#amethyst"];
 
-var wins = 0;
-//Print wins
-$("#win-counter").append(wins);
+    //Setup new game
+    function setup() {
+        //Generate a random number at the start of the game between 19 and 120
+        random = 19 + Math.floor(Math.random() * 120);
+        //Print random number
+        $("#random").html(random);
+        
+        //Loop through crystals array
+        for (var i = 0; i < crystals.length; i++) {
+            //Generate random number between 1 and 12
+            var value = Math.floor(Math.random() * 12) + 1;
+            //Assign value to each crystal
+            $(crystals[i]).attr("data-value", value);
+        }
 
-var losses = 0;
-//Print losses
-$("#loss-counter").append(losses);
+        score = 0;
+        $("#score").text("Score: " + score);
+    };
 
-//Generate a random number at the start of the game between 19 and 120
-function generate() {
-    var random = 19 + Math.floor(Math.random() * 120);
-    return random;
-}
+    //Write a click handler to take the value from each crystal that is clicked and updated the score 
+    $(document).on("click", ".crystals", function() {
+        score += parseInt($(this).attr("data-value"));
+        $("#score").text("Score: " + score);
+        //Check if score is greater than random and update losses
+        if (score > random) {
+            losses += 1;
+            //Print losses
+            $("#loss-counter").html(losses);
+            alert("You lost!");
+            //Reset game
+            setup();
+        }
+        else if (score === random) {
+            wins += 1;
+            //Print wins
+            $("#win-counter").append(wins);
+            alert("You win!");
+            //Reset game
+            setup();
+        }
 
-//Print random number
-$("#random").append(generate());
+    });
 
-//Each crystal should have a random hidden value between 1 - 12.
-var emerald = Math.floor(Math.random() * 12) + 1;
-var turquoise = Math.floor(Math.random() * 12) + 1;
-var diamond = Math.floor(Math.random() * 12) + 1;
-var amethyst = Math.floor(Math.random() * 12) + 1;
+    setup();
 
-//When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-$("#emerald").on("click", function() {
-    score += emerald;
-    $("#score").text("Score: " + emerald);
-  });
+});
